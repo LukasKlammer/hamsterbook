632a794f40790de2735487eae79b17911a71ecda
LukasKlammer <klammer.lukas@hotmail.com>
2022-05-29 19:40:50 +0200

    create hamster-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HamsterCardComponent } from './hamster-card/hamster-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HamsterCardComponent
   ],
   imports: [
     BrowserModule,
