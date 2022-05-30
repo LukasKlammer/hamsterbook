ca589b744fd11c5c5dff2a0266ea352081a662cc
Lukas Klammer <klammer.lukas@hotmail.com>
2022-05-30 14:52:43 +0200

    image and text input output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hamsterbook';
+  
+  postText = [
+    'Hallo, mein Name ist Frederick! Ich bin hier, um neue Haster-Freunde kennenzulernen.', 
+    'Hallo, mein Name ist Hamsti! Ich bin hier, um mich über neueste HamsterApps auszutauschen.', 
+    'Hi Leute! Ich bin neu hier und freue mich über eure Likes.', 
+    'Top Seite! Freue mich, dabei zu sein!', 
+    'Hi! Ich bin Wuschel! Der coolste Hamster aller Zeiten.'
+  ];
+
+  hamsterImages = [
+    'assets/img/hamster/hamster_1.jpg',
+    'assets/img/hamster/hamster_2.jpg',
+    'assets/img/hamster/hamster_3.jpg',
+    'assets/img/hamster/hamster_4.jpg',
+    'assets/img/hamster/hamster_5.jpg',
+    'assets/img/hamster/hamster_6.jpg',
+    'assets/img/hamster/hamster_7.jpg',
+    'assets/img/hamster/hamster_8.jpg',
+  ];
 
   buyHamster() {
     alert('Hamster bestellt!');
--- src/app/hamster-card/hamster-card.component.ts
+++ src/app/hamster-card/hamster-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hamster-card',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hamster-card.component.scss']
 })
 export class HamsterCardComponent implements OnInit {
+
+  @Input() text:string = 'Noch keine Eigenschaften';
+  @Input() img:string = 'Noch keine Eigenschaften';
 
   constructor() { }
 
