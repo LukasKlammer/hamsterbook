0c28663d0e6ff437a250fc37964c897bcb433d8e
LukasKlammer <klammer.lukas@hotmail.com>
2022-06-01 19:35:12 +0200

    render proposal images, overgive array in components

--- src/app/profile-row/profile-row.component.ts
+++ src/app/profile-row/profile-row.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile-row',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-row.component.scss']
 })
 export class ProfileRowComponent implements OnInit {
+
+  @Input() name = 'Frederick';
+  @Input() img = 'assets/img/hamster/hamster_1.jpg';
+  @Input() description = '3 Jahre alt';
 
   constructor() { }
 
--- src/app/proposals/proposals.component.ts
+++ src/app/proposals/proposals.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-proposals',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./proposals.component.scss']
 })
 export class ProposalsComponent implements OnInit {
+
+  @Input() hamsterImages:any = [];
+
+  names = ['Marius', 'Franziska'];
+  texts = ['Hallo Welt', 'Hallo Welt 2'];
 
   constructor() { }
 
